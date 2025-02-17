42e8e0b521a17751449a387d64e60ecd5b2efbcd
Marian Trpkos <majda.trpkos@gmail.com>
2025-02-17 11:42:52 +0100

    add leftSpecial and rightSpecial directions for super action

--- Sources/Shuffle/Classes/SwipeDirection.swift
+++ Sources/Shuffle/Classes/SwipeDirection.swift
@@ -27,37 +27,48 @@ import UIKit
 /// A type representing the direction of a physical drag.
 @objc
 public enum SwipeDirection: Int, CustomStringConvertible {
-  // swiftlint:disable:next identifier_name
-  case left, right, up, down
-
-  /// A static array containing all `SwipeDirection`s.
-  public static let allDirections: [SwipeDirection] = [left, up, right, down]
-
-  /// The `SwipeDirection` represented as a unit vector.
-  public var vector: CGVector {
-    switch self {
-    case .left:
-      return CGVector(dx: -1, dy: 0)
-    case .right:
-      return CGVector(dx: 1, dy: 0)
-    case .up:
-      return CGVector(dx: 0, dy: -1)
-    case .down:
-      return CGVector(dx: 0, dy: 1)
+    // swiftlint:disable:next identifier_name
+    case left, right, up, down
+    case leftSpecial, rightSpecial
+    
+    /// A static array containing all `SwipeDirection`s.
+    public static let allDirections: [SwipeDirection] = [left, up, right, down]
+    
+    /// The `SwipeDirection` represented as a unit vector.
+    public var vector: CGVector {
+        switch self {
+        case .left:
+            return CGVector(dx: -1, dy: 0)
+        case .right:
+            return CGVector(dx: 1, dy: 0)
+        case .up:
+            return CGVector(dx: 0, dy: -1)
+        case .down:
+            return CGVector(dx: 0, dy: 1)
+            
+        case .leftSpecial:
+            return CGVector(dx: -1, dy: 0)
+        case .rightSpecial:
+            return CGVector(dx: 1, dy: 0)
+        }
     }
-  }
-
-  /// A textual representation of the `SwipeDirection`.
-  public var description: String {
-    switch self {
-    case .left:
-      return "left"
-    case .right:
-      return "right"
-    case .up:
-      return "up"
-    case .down:
-      return "down"
+    
+    /// A textual representation of the `SwipeDirection`.
+    public var description: String {
+        switch self {
+        case .left:
+            return "left"
+        case .right:
+            return "right"
+        case .up:
+            return "up"
+        case .down:
+            return "down"
+            
+        case .leftSpecial:
+            return "leftSpecial"
+        case .rightSpecial:
+            return "rightSpecial"
+        }
     }
-  }
 }
